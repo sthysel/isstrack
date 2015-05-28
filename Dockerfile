9c8537d0e8f8efeea78bed68b6a88813785582da
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2015-05-28 23:19:11 +0800

    Init

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.4-onbuild
+MAINTAINER sthysel <sthyse@gmail.com>
+
+ENV REFRESHED_AT 2015-05-27
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    ca-certificates \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+RUN env --unset=DEBIAN_FRONTEND
+
+CMD ["/bin/bash"]
---
+++ isstrack.py
@@ -0,0 +1,31 @@
+import requests
+import time
+from math import asin, cos, radians, sin, sqrt
+
+url = "https://api.wheretheiss.at/v1/satellites/25544"
+
+# lat, long
+home_pos = (45.686, -121.566)
+
+
+def get_iis_position():
+    res = requests.get(url, verify=False).json()
+    iss_pos = (res['latitude'], res['longitude'])
+    dist = get_spherical_distance(home_pos, iss_pos)
+    return dist
+
+
+def get_spherical_distance(pos1, pos2, radius=6373):
+    """ Returns distance on sphere between points given as (latitude, longitude) in degrees. """
+
+    lat1 = radians(pos1[0])
+    lat2 = radians(pos2[0])
+    dLat = lat2 - lat1
+    dLon = radians(pos2[1]) - radians(pos1[1])
+    a = sin(dLat / 2.0) ** 2 + cos(lat1) * cos(lat2) * sin(dLon / 2.0) ** 2
+    return 2 * asin(min(1, sqrt(a))) * radius
+
+
+while True:
+    print(get_iis_position())
+    time.sleep(5)
